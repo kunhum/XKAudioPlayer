9bbba919d186b8a4d1540d6a0288efbb866d68db
kenneth <kunhum@163.com>
2022-05-26 16:37:13 +0800

    初始化

---
+++ Example/XKAudioPlayer/OCViewController.h
@@ -0,0 +1,17 @@
+//
+//  OCViewController.h
+//  XKAudioPlayer_Example
+//
+//  Created by kenneth on 2022/5/23.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OCViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XKAudioPlayer/Classes/XKAudioPlayer.swift
@@ -0,0 +1,12 @@
+//
+//  XKAudioPlayer.swift
+//  XKAudioPlayer
+//
+//  Created by kenneth on 2022/5/23.
+//
+
+import UIKit
+
+class XKAudioPlayer: NSObject {
+
+}
